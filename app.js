81d66eeeb7a835736cd35ab0d14b20db73f1ddc3
Lamsauce <lilylam743@gmail.com>
2023-11-25 19:31:54 -0800

    lil bit more lol

---
+++ app.js
@@ -0,0 +1,95 @@
+
+/*
+SCARLET VIOLET
+- Shiny Charm
+- Masuda Method
+- Outbreak KOs
+- Sparkling Power Level
+- Wild Tera Pokemon
+- Tera Raid Den (*Tera Raid Den shiny odds are unable to be increased, and are higher than normal.)
+*/
+
+/*
+POKEMON LEGENDS: ARCEUS
+- Shiny Charm
+- Pokedex Entry
+- Outbreak Type
+*/
+
+/*
+POKEMON BDSP
+- Shiny Charm (The Shiny Charm in BDSP only affects breeding.)
+- Masuda Method
+- Pokeradar Chain
+- 40 Diglett Bonus
+*/
+
+/*
+POKEMON SWORD/SHIELD
+- Shiny Charm
+- Masuda Charm
+- Brilliant Pokemon
+- No. Battled
+- Dynamax Adventure
+- Max Raid Den/Regis
+*/
+
+/*
+POKEMON LGPE
+- Shiny Charm
+- Lure
+- Catch Combo (The Catch Combo only applies to the next Pokemon of the chaned species to spawn in.)
+- Trade/Gift/Static
+*/
+
+/*
+POKEMON USUM | UM
+- Shiny Charm
+- Masuda Method
+- SOS Chain Length (UM, chain surpasses 255, resets to 0)
+- Wormhole Rings
+- Lightyears Travelled
+- Gift/UB/Legendary
+
+(**The option "2 + ꕤ" refers to the 'blooming' wormholes that always spawn uncaught legendaries)
+*/
+
+/*
+POKEMON ORAS
+- Shiny Charm
+- Masuda Charm
+- DexNav Chain (*These odds are combined with the regular odds of finding a wild shiny with/without the charm.)
+- Search Level (**There is a random 4% chance that you will roll for a shiny an additional 4 times while DexNav hunting. This is calculated for.)
+- Chain Fishing
+*/
+
+/*
+POKEMON X/Y
+- Shiny Charm
+- Masuda Method
+- PokeRadar Chain (*Exact odds unknown. Odds are calculated how they were in Gen 4, but modified to fit the new odds.)
+- Friend Safari
+- Chain Fishing
+*/
+
+/*
+POKEMON B2W2 | BW
+- Shiny Charm (*Shiny charm is only available in Black 2 and White 2.)
+- Masuda Method
+- Trade/Gift
+*/
+
+/*
+HGSS | PLATINUM | DP
+- Masuda Method (*Calculates odds of a shiny patch appearing. Cute Charm glitch does not increase patch odds.)
+- Poke Radar Chain
+- Cute Charm Glitch (**Odds are approximate and unconfirmed. Calculates the best possible odds when using this glitch.)
+*/
+
+/*
+CRYSTAL | GOLD AND SILVER
+- Odd Egg
+- Specific Pokemon (*Each species has a different chance of being shiny. Select N/A for odds of getting any shiny at all.)
+- Shiny Parent (**Genderless Pokemon can only breed with Ditto. If Ditto is not shiny, your odds remaind full.)
+- Gender Ratio (***The offspring's gender ratio. Select 50/50 for the Nido family.)
+*/
